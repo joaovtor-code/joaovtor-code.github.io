e7d362752b5071f07bf91678042f0d10d6f62bba
joaovtor-code <69129505+joaovtor-code@users.noreply.github.com>
2020-08-03 12:16:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 fetch("https://api.edmodo.com/stream.replies_threaded_json?request_origin=react-web-app&page=1&per_page=5&item_types=note,poll,assignment,quiz,app_message,video,template_message,activity_studio&template_types=basic,banner,promo&user_conversations=false&exclude_creator_id=false&context=home&num_replies=1", {
-    "credentials": "include",
+    "credentials": "omit",
     "headers": {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:79.0) Gecko/20100101 Firefox/79.0",
         "Accept": "*/*",
